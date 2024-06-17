5b0b9be34073e88b92fbf5684cdc477547a6fd0f
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-17 19:49:20 -0300

    up

--- Módulo 1/#02 Variáveis/como usar/script.js
+++ Módulo 1/#02 Variáveis/como usar/script.js
@@ -1,6 +1,12 @@
 let nome = "André Bredofw"
 let idade = 18
+let cidade = "Pinheiros"
+let estado = "Espirito Santo"
 
 console.log(nome)
 
 console.log(idade)
+
+console.log(cidade)
+
+console.log(estado)
---
+++ Módulo 1/#03 tipos de dados/script.js
@@ -0,0 +1,11 @@
+let nome = "André"
+
+ let idade = 18
+
+ let logado = true
+
+ let lista = ["ovo", "massa", "corante"]
+
+ let nomeCompleto = { nome: "Bonieky", idade: 90}
+
+ console.log(typeof nomeCompleto)
