aa8c3f28478afe6ff26441779420d641ee072222
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-19 19:55:23 -0300

    up

---
+++ Módulo 1/#06 template string/script.js
@@ -0,0 +1,10 @@
+let nome = "André"
+let sobrenome = "Bredofw"
+
+letCompleto = `Nome: ${nome} ${sobrenome} `
+
+console.log(letCompleto)
+
+let idade = 18
+let iidade = `Idade: ${idade}`
+console.log(iidade)
