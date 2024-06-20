214886bb451dd9ea79e60838c97e59877849c47d
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-20 19:44:27 -0300

    up

---
+++ Módulo 1/#08 multicondicionas/script.js
@@ -0,0 +1,13 @@
+let idade = 18
+
+ /* if (idade >=18){
+        if(idade < 60){
+        console.log("Você é maior de idade!")
+    }
+        se a idade for maior ou igual a 18 irá entrar no primeiro if, em seguida irá entrar no segundo if onde analisará se a idade é menor que 60, caso seja, exibirá mensagem no console
+ } */
+
+        if (idade >=18 && idade < 60) {
+            console.log("Você é um adulto!")
+            
+        }
