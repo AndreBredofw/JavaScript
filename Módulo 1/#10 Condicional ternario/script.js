f71ab74127fa45d494ca47201cb225724cfcd586
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-23 17:56:42 -0300

    up

---
+++ Módulo 1/#10 Condicional ternario/script.js
@@ -0,0 +1,11 @@
+let isMember = true;
+
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember ? 'Você é membro!' : 'Você não é membro!');
+
+console.log("O frete será: " + shipping)
+
+let age = 18;
+let isAdult = (age >= 18 ? true : false);
+console.log("Você é adulto? resposta: " + isAdult )
