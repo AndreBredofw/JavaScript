ba24264714e61a4ac8e77a7d5d1aa2ad714b265b
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-24 20:18:20 -0300

    Update script.js

--- Módulo 2/#02 passando parametros em funcoes/script.js
+++ Módulo 2/#02 passando parametros em funcoes/script.js
@@ -3,3 +3,9 @@ function somar(n1, n2){
     console.log('Resultado: ' + resultado);
 }
 somar(10, 15)
+
+function nomeCompleto(nome, sobrenome){
+    console.log(`${nome} ${sobrenome}`);
+}
+
+nomeCompleto("André", "Bredofw")
