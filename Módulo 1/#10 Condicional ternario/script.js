aa8d69ea0dbbbf24ae12c54e4e57643dc35c0c4a
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-26 19:16:32 -0300

    Update script.js

--- Módulo 1/#10 Condicional ternario/script.js
+++ Módulo 1/#10 Condicional ternario/script.js
@@ -9,3 +9,7 @@ console.log("O frete será: " + shipping)
 let age = 18;
 let isAdult = (age >= 18 ? true : false);
 console.log("Você é adulto? resposta: " + isAdult )
+
+let idade = 17;
+let verificacao= (idade >=18 ? 'Você é maior de idade!' : 'Você ainda é de menor!');
+console.log(verificacao)
