23217b32f1e11ffea15a557b9866e5e99f96d3aa
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-06-26 19:55:35 -0300

    up

---
+++ Módulo 2/#03 retornando uma funcao/script.js
@@ -0,0 +1,13 @@
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
+}
+
+console.log(nomeCompleto("André", "Bredofw"));
+
+function retornaX(x){
+    x = 9;
+    return x + 2;
+}
+
+const X = retornaX();
+console.log(X) 
