33ca5bf4031e293a7dae2dbd76b41faf1ebef366
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-07-01 19:26:40 -0300

    up

---
+++ Módulo 2/#06 arrow function/script.js
@@ -0,0 +1,21 @@
+
+/*  function somar(x,y) {
+    return x + y;
+} */
+
+    const somar = (x, y) =>{
+        return x + y;
+    }
+console.log(somar(10,5))
+
+let cc = 0;
+
+function add() {
+    let cc = 0;
+    cc++;
+}
+
+add();
+add();
+
+console.log(cc); //o resultado será 0 pois está sendo puxado da variavel de escopo global, caso ti re a varivale de dentro da funcao, a funcao iria usar a variavel global para fazer a soma.
