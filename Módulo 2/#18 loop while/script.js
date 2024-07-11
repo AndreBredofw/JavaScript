937e3393ce9848d0178a03cbc6fa95927345c59a
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-07-11 19:55:51 -0300

    up

---
+++ Módulo 2/#18 loop while/script.js
@@ -0,0 +1,10 @@
+let numero  = 0;
+
+while (numero  < 10){
+    console.log(`O número da ver é ${numero}`);
+    numero++;
+}
+console.log(" ")
+for(let numero = 0; numero <= 7; numero++){
+console.log(`O número da vez é  ${numero}`);
+}
