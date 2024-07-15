2f8c73a37cb7f8ec9fa144de4b2710562e0a3535
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-07-15 20:21:13 -0300

    up

---
+++ Módulo 2/revisando conteudos/#02 array e objetos/script.js
@@ -0,0 +1,14 @@
+let personagem = {
+   nome: 'André',
+   sobrenome: 'Bredofw',
+   forca: 55,
+   velocidade: 88
+};
+
+console.log(personagem.nome);
+console.log(personagem.sobrenome);
+
+let nomes = ['André', 'João', 'Lucidreia', 'Janira', 'João'];
+nomes.push('XXXX');
+console.log(`Total de itens no array: ${nomes.length}`);
+
