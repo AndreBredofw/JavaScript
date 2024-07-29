a14a064166d30e080073de2e43e2637f7277582f
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-07-29 18:38:53 -0300

    Update script.js

--- Módulo 2/#22 iteração de array/script.js
+++ Módulo 2/#22 iteração de array/script.js
@@ -3,5 +3,15 @@ let fruits = ['Banana', 'Laranja', 'Maçã', 'Pêra'];
 let bigFruits = fruits.filter((item) => {
     return item.length > 4;
 });
-
 console.log(bigFruits);
+
+let ok = fruits.every((qualquer) => {
+    return qualquer.length > 3;
+
+});
+
+if (ok) {
+    console.log("Todos são maiores do que 3");
+} else{
+    console.log("Não são todos maiores do que 3");
+};
