d0ab89873650f08752daa580414a6a665fde6a8b
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-07-30 22:54:19 -0300

    Update script.js

--- Módulo 3/selecionando elementos 2/script.js
+++ Módulo 3/selecionando elementos 2/script.js
@@ -1,2 +1,8 @@
 document.getElementsByTagName("h1");
 /*Filtra todos os h1*/ 
+
+document.getElementById("teste");
+// filtra pelo nome do id (unico)
+
+document.getElementsByClassName("botão");
+// filtra pelo nome da classe (pode se repetir)
