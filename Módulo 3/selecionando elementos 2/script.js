ba708ac24665a4be6e14233b9756a7239821abb8
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-07-31 19:55:08 -0300

    up

--- Módulo 3/selecionando elementos 2/script.js
+++ Módulo 3/selecionando elementos 2/script.js
@@ -6,3 +6,12 @@ document.getElementById("teste");
 
 document.getElementsByClassName("botão");
 // filtra pelo nome da classe (pode se repetir)
+
+
+document.querySelector("#teste");
+document.querySelector(".botao");
+// pega os dados exatamente como fazemos no css com as classes, id's e etc.
+
+document.querySelector("li");
+
+document.querySelectorAll("#teste2>ul>li");
