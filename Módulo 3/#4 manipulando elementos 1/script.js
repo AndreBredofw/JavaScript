39850baf581e659b5b1aa1d37659f3c59ca78eb8
dev_andre <90481786+AndreBredofw@users.noreply.github.com>
2024-08-04 17:40:40 -0300

    Update script.js

--- Módulo 3/#4 manipulando elementos 1/script.js
+++ Módulo 3/#4 manipulando elementos 1/script.js
@@ -3,5 +3,9 @@ function clicou() {
   // console.log(teste.children[1].children);
   const ul = teste.querySelector('ul');
   //console.log(ul.innerHTML);
-  ul.innerHTML = "<li>Item alterado<li>";
+  //ul.innerHTML = ul.innerHTML + "<li>Item alterado<li>";
+  ul.children[0].innerHTML = "Modificação";
+
+  //ul.outerHTML = "Alterado"
+  //console.log(ul.outerHTML)
 }
